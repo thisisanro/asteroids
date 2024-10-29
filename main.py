4bea73c9fed07ed4a4e7f1d7913120fee2f6401c
thisisanro <antonromanenko@tutanota.com>
2024-10-29 18:28:54 +0000

    add black screen

--- main.py
+++ main.py
@@ -5,9 +5,20 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        #exit program by mouse
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        #fill the screen with black
+        screen.fill((0, 0, 0))
+        
+        #update display
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
